add453fb42456d3abf7a7e78fa7b1b0b0cd002fa
AdrianS2002 <aschiop2002@yahoo.com>
2023-11-09 20:04:43 +0200

    clase noi, clase builder

--- src/main/java/model/AudioBook.java
+++ src/main/java/model/AudioBook.java
@@ -1,2 +1,54 @@
-package model;public class AudioBook {
+package model;
+
+import java.time.LocalDate;
+
+public class AudioBook {
+
+    private Long id;
+
+    private String author;
+
+    private String title;
+
+    private LocalDate publishedDate;
+
+    private int runtime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {this.publishedDate = publishedDate;}
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
 }
